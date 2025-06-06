41bd17335e5df67e60069645ba4b3fdbbde70ee4
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-06-05 23:55:09 -0500

    Agregar color primario y secundarios en la configuración de Tailwind CSS; ajustar el texto en el componente App.

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,9 @@ const App = () => {
   return (
     <SafeAreaView>
       <View className="mt-6 mx-2.5">
-        <Text className="text-3xl font-work-black">Hola mundo</Text>
+        <Text className="text-3xl font-work-black text-primary">
+          Hola mundo
+        </Text>
       </View>
     </SafeAreaView>
   );
