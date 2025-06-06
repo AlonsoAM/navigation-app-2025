4a17e506c7cecc9bc52e7678c8eeda93b5bc09fe
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-06-05 23:49:59 -0500

    Integrate refineAreaView
    Added support for Work Sans fonts and integrated font loading-font in `_layout.tsx`, ensuring proper splash screen handling.View and adjusted styles. custom font families inwind.config.js` for consistent usage.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./global.css";
-import { Slot } from "expo-router";
+import { Slot, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+    "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    } else if (error) {
+      console.error("Error loading fonts:", error);
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null; // or a loading indicator
+  }
+
   return <Slot />;
 };
 
